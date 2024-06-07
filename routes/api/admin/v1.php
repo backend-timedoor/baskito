48c33d537ecbccf42c04e2b540b6465edc6d17d0
Aryan Jaya <aryanjaya88@gmail.com>
2024-06-07 15:41:06 +0800

    add user permission api in admin api

--- routes/api/admin/v1.php
+++ routes/api/admin/v1.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\Admin\V1\Auth\ChangePasswordController;
 use App\Http\Controllers\API\Admin\V1\Auth\LoginController;
 use App\Http\Controllers\API\Admin\V1\Auth\MeController;
 use App\Http\Controllers\API\Admin\V1\Auth\ProfileController;
+use App\Http\Controllers\API\Admin\V1\Auth\UserPermissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [LoginController::class, 'login'])->name('auth.login');
@@ -15,5 +16,6 @@ Route::middleware('auth:api-admin')->group(function () {
         Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
         Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::post('password', [ChangePasswordController::class, 'update'])->name('password.update');
+        Route::get('permission', [UserPermissionController::class, 'index'])->name('permission.index');
     });
 });
