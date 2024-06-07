a3296065bfe5163f687963696550f4ec9daebc71
Aryan Jaya <aryanjaya88@gmail.com>
2024-06-07 14:35:58 +0800

    add helper to cast mixed to string

--- app/Supports/helpers.php
+++ app/Supports/helpers.php
@@ -19,3 +19,24 @@ if (! function_exists('auth_user')) {
         return $user;
     }
 }
+
+if (! function_exists('cast_to_string')) {
+    /**
+     * cast value to string
+     *
+     * @param  mixed  $value
+     */
+    function cast_to_string($value): string
+    {
+        if (
+            is_string($value) ||
+            is_scalar($value) || // Check if the variable is a scalar type (int, float, bool, string)
+            is_null($value) ||
+            (is_object($value) && method_exists($value, '__toString'))
+        ) {
+            return (string) $value;
+        }
+
+        throw new InvalidArgumentException('value cannot be casted to string');
+    }
+}
